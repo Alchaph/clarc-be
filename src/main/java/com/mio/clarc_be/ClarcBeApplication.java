514e333799c1029f43c766480ee032834ab28771
Marc Sutthaloung <marc.sutthaloung@gmx.net>
2024-11-20 17:12:11 +0100

    initial commit

---
+++ src/main/java/com/mio/clarc_be/ClarcBeApplication.java
@@ -0,0 +1,13 @@
+package com.mio.clarc_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClarcBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClarcBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mio/clarc_be/ClarcBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mio.clarc_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClarcBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
