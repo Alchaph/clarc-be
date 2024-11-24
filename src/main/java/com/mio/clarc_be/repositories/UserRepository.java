c9a01e531d8f23f8e6138db1ad78671fffea92d9
Marc Sutthaloung <marc.sutthaloung@gmx.net>
2024-11-24 16:49:41 +0100

    jwt login and signup methods

---
+++ src/main/java/com/mio/clarc_be/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.mio.clarc_be.repositories;
+
+import com.mio.clarc_be.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
